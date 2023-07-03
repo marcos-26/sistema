60c1bb49b94fad73261370ee6876d121a1892581
Marcos Paulo Rodrigues Rodrigues <m.rodrigues@vertex.vertexdigital.co>
2023-07-03 10:49:18 -0300

    melhorias

--- app/Models/Clientes.php
+++ app/Models/Clientes.php
@@ -39,4 +39,163 @@ class Clientes extends ClientesRepository
         return $this;
     }
 
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nascimento
+     */
+    public function getNascimento()
+    {
+        return $this->nascimento;
+    }
+
+    /**
+     * Set the value of nascimento
+     *
+     * @return  self
+     */
+    public function setNascimento($nascimento)
+    {
+        $this->nascimento = $nascimento;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cpf
+     */
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    /**
+     * Set the value of cpf
+     *
+     * @return  self
+     */
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of telefone
+     */
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * Set the value of telefone
+     *
+     * @return  self
+     */
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of endereco
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * Set the value of endereco
+     *
+     * @return  self
+     */
+    public function setEndereco($endereco)
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of uf
+     */
+    public function getUf()
+    {
+        return $this->uf;
+    }
+
+    /**
+     * Set the value of uf
+     *
+     * @return  self
+     */
+    public function setUf($uf)
+    {
+        $this->uf = $uf;
+
+        return $this;
+    }
 }
---
+++ app/Models/Enum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Model;
+
+abstract class Enum
+{
+    const NOME = 'Nome';
+    const NASCIMENTO = 'Nascimento';
+    const CPF = 'CPF';
+    const EMAIL = 'Email';
+    const TELEFONE = 'Telefone';
+    const UF = 'UF';
+    const ENDERECO = 'Endereço';
+}
