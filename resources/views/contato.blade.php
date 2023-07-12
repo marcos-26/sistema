d66a8b0da3d0af5f110a4795c5c3cb9b82ef4e0a
Marcos Paulo Rodrigues Rodrigues <m.rodrigues@vertex.vertexdigital.co>
2023-07-12 16:34:17 -0300

    aplica responsividade

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -21,6 +21,10 @@
                   <label for="exampleInputPassword1" class="form-label">Password</label>
                   <input type="password" class="form-control" id="exampleInputPassword1">
                 </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
                 <div class="mb-3 form-check">
                   <input type="checkbox" class="form-check-input" id="exampleCheck1">
                   <label class="form-check-label" for="exampleCheck1">Check me out</label>
