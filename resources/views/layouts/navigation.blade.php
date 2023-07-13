c2475c5005e8eb49feba29bc68d62fadc3fae80b
Marcos Paulo Rodrigues Rodrigues <m.rodrigues@vertex.vertexdigital.co>
2023-07-13 01:01:44 -0300

    criando pages

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,13 +25,25 @@
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Leads') }}
                     </x-nav-link>
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('contato')" :active="request()->routeIs('contato')">
                         {{ __('Contatos') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('contato')" :active="request()->routeIs('contato')">
+                        {{ __('Vendas') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('contato')" :active="request()->routeIs('contato')">
+                        {{ __('Relatorio') }}
                     </x-nav-link>
                 </div>
 
