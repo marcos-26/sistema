4f74001ad2c00b5bd78cbe88a2cfed54ebe8197c
Marcos Paulo Rodrigues Rodrigues <m.rodrigues@vertex.vertexdigital.co>
2023-07-13 02:40:42 -0300

    segmenta rotas

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Lead extends Model
 {
     use HasFactory;
+    public static function factory(): Lead
+    {
+        return app()->make(Lead::class);
+    }
+
+    public function populate(array $data): self
+    {
+        $this->id = $data['id'] ?? null;
+        $this->nome = $data['nome'];
+        $this->nascimento = $data['nascimento'];
+        return $this;
+    }
 }
--- resources/views/lead.blade.php
+++ resources/views/lead.blade.php
@@ -5,4 +5,83 @@
             {{ __('Leads') }}
         </h2>
     </x-slot>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 text-gray-900">
+            <div class="table-responsive">
+                <table class="table" id="search">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Nascimento</th>
+                            <th scope="col">Cpf</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Telefone</th>
+                            <th scope="col">Endereço</th>
+                            <th scope="col">UF</th>
+                            <th></th>
+                          </tr>
+                    </thead>
+                    <tbody>
+                        @if (isset($search))
+                        @foreach ($search as $cliente)
+                        <tr>
+                            <td data-label="Job Title">
+                            <a class="text-heading font-semibold" href="#">
+                                {{$cliente->nome}}
+                            </a>
+                            </td>
+                            <td data-label="Nascimento">
+                            <span>{{$cliente->nascimento}}</span>
+                            </td>
+                            <td data-label="Cpf">
+                            <a class="text-current">{{$cliente->cpf}}</a>
+                            </td>
+                            <td data-label="Email">
+                            <a class="text-current">{{$cliente->email}}</a>
+                            </td>
+                            <td data-label="Telefone">
+                            <span class="badge bg-soft-success text-success">{{$cliente->telefone}}</span>
+                            </td>
+                            <td data-label="Endereço">
+                            <a class="text-current">{{$cliente->endereco}}</a>
+                            </td>
+                            <td data-label="Uf">
+                            <a class="text-current">{{$cliente->uf}}</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @else
+                         @foreach ($clientes as $cliente)
+                         <tr>
+                             <td data-label="Job Title">
+                             <a class="text-heading font-semibold" href="#">
+                                 {{$cliente->nome}}
+                             </a>
+                             </td>
+                             <td data-label="Nascimento">
+                             <span>{{$cliente->nascimento}}</span>
+                             </td>
+                             <td data-label="Cpf">
+                             <a class="text-current">{{$cliente->cpf}}</a>
+                             </td>
+                             <td data-label="Email">
+                             <a class="text-current">{{$cliente->email}}</a>
+                             </td>
+                             <td data-label="Telefone">
+                             <span class="badge bg-soft-success text-success">{{$cliente->telefone}}</span>
+                             </td>
+                             <td data-label="Endereço">
+                             <a class="text-current">{{$cliente->endereco}}</a>
+                             </td>
+                             <td data-label="Uf">
+                             <a class="text-current">{{$cliente->uf}}</a>
+                             </td>
+                         </tr>
+                         @endforeach
+                       @endif
+                    </tbody>
+                  </table>
+              </div>
+        </div>
+    </div>
 </x-app-layout>
