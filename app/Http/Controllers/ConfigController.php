44738714f11fd8b120285c50ab8bbff75ca3410c
Marcos Paulo Rodrigues Rodrigues <m.rodrigues@vertex.vertexdigital.co>
2023-07-13 21:04:40 -0300

    continua construção objetos

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Config;
 use Illuminate\Http\Request;
 
 class ConfigController extends Controller
 {
     public function config()
     {
+        $configuracao = Config::all();
+        return view('configuracao', ['configuracao' => $configuracao]);
         return view('configuracao');
     }
 }
--- app/Models/Config.php
+++ app/Models/Config.php
@@ -2,10 +2,45 @@
 
 namespace App\Models;
 
+use App\Domain\Repository\ConfigRepository;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Config extends ConfigRepository
 {
     use HasFactory;
+     /** @attributes -
+     * $id
+     */
+
+     public static function factory(): Config
+     {
+         return app()->make(Config::class);
+     }
+
+     public function populate(array $data): self
+     {
+         $this->id = $data['id'] ?? null;
+         return $this;
+     }
+
+     /**
+      * Get the value of id
+      */
+     public function getId()
+     {
+          return $this->id;
+     }
+
+     /**
+      * Set the value of id
+      *
+      * @return  self
+      */
+     public function setId($id)
+     {
+          $this->id = $id;
+
+          return $this;
+     }
 }
